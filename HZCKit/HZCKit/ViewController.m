ec2b5255dcf2a039f96f6ccef2a9f9fc9e655a55
huangzhicheng <13826457912@163.com>
2018-08-02 09:33:58 +0800

    初始化项目

---
+++ HZCKit/HZCKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HZCKit
+//
+//  Created by Apple on 2018/8/2.
+//  Copyright © 2018年 AiChen smart Windows and doors technology co., LTD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
